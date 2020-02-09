450890982215613c8863e5f8bc5604b7d4b759c4
toupenator <abdullah.baig@hotmail.com>
2020-02-09 02:36:58 -0500

    Camera X and FireBase ML Kit Implimented for barcode reading. If this doesn't work imma yeet myself out the window

---
+++ QStamps/app/src/main/java/com/example/qstamps/BarcodeScanner/QCodeAnalyzer.java
@@ -0,0 +1,91 @@
+package com.example.qstamps.BarcodeScanner;
+
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.media.Image;
+
+import androidx.annotation.NonNull;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+
+import java.io.IOException;
+import java.util.List;
+
+public class QCodeAnalyzer implements ImageAnalysis.Analyzer{
+    private int degreesToFirebaseRotation(int degrees) {
+        switch (degrees) {
+            case 0:
+                return FirebaseVisionImageMetadata.ROTATION_0;
+            case 90:
+                return FirebaseVisionImageMetadata.ROTATION_90;
+            case 180:
+                return FirebaseVisionImageMetadata.ROTATION_180;
+            case 270:
+                return FirebaseVisionImageMetadata.ROTATION_270;
+            default:
+                throw new IllegalArgumentException(
+                        "Rotation must be 0, 90, 180, or 270.");
+        }
+    }
+
+    @Override
+    public void analyze(ImageProxy imageProxy, int degrees) {
+        if (imageProxy == null || imageProxy.getImage() == null) {
+            return;
+        }
+        Image mediaImage = imageProxy.getImage();
+        int rotation = degreesToFirebaseRotation(degrees);
+        FirebaseVisionImage image =
+                FirebaseVisionImage.fromMediaImage(mediaImage, rotation);
+        FirebaseVisionBarcodeDetector detector = FirebaseVision.getInstance()
+                .getVisionBarcodeDetector();
+
+        Task<List<FirebaseVisionBarcode>> result = detector.detectInImage(image)
+                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
+                    @Override
+                    public void onSuccess(List<FirebaseVisionBarcode> barcodes) {
+                        for (FirebaseVisionBarcode barcode: barcodes) {
+                            Rect bounds = barcode.getBoundingBox();
+                            Point[] corners = barcode.getCornerPoints();
+
+                            String rawValue = barcode.getRawValue();
+
+                            int valueType = barcode.getValueType();
+                            // See API reference for complete list of supported types
+                            switch (valueType) {
+                                case FirebaseVisionBarcode.TYPE_TEXT:
+                                    String text = barcode.getRawValue();
+                                    onRead(text);
+                                    break;
+
+                            }
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Task failed with an exception
+                        // ...
+                    }
+                });
+
+
+
+    }
+    //text passed to flask python
+    public void onRead(String text){
+
+    }
+
+
+}
